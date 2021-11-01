1bdad3c7e6b77b87916ed47e560cccb297536aee
abobik <kbchmaster@gmail.com>
2021-11-01 18:27:06 +0000

    rpi testing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import config
 from dev_sim7000 import *
 from dev_stemma import *
@@ -7,21 +8,32 @@ import asyncio
 
 
 if __name__ == '__main__':
-    # iot_mod.initialize_serial()
-    # # iot_mod.initialize_apn()
-    #
+    iot_mod.initialize_serial()
+    # iot_mod.initialize_apn()
+    # #
     # current_time = iot_mod.send_GET_request(url=f'{config.API_URL}/timesync')
     # print('GET MESSAGE', current_time)
     # #
-    # payload = '{"mac_address": "00:00:00:00:00:00", "temperature": "22", "humidity": "33", "pressure": "888", "adc": "4.1"}'
+    payload = '{"mac_address": "00:00:00:00:00:00", "temperature": "22", "humidity": "33", "pressure": "888", "adc": "4.1"}'
     # # # payload = 'mac_address=00%3A00%3A00%3A00%3A00%3A00&temperature=11&humidity=22&pressure=777&adc=3.9'
     # #
-    # iot_mod.send_POST_request(url=f'{config.API_URL}', payload=payload, content='JSON')
+    iot_mod.send_POST_request(url=f'{config.API_URL}', payload=payload, content='JSON')
     # SIM7000.send_sms(phone_no=config.PHONE_NO, msg=payload)
     #
-    # iot_mod.deinitialize_serial()
 
     SOIL_SENSORS = 1
     ss_sensors = {f'ss_sensor_{hex(addr)}': STEMMA(addr=addr) for addr in range(0x36, 0x36 + SOIL_SENSORS)}
-    print(ss_sensors['ss_sensor_0x36'].get_moisture())
 
+    while True:
+        humidity = ss_sensors['ss_sensor_0x36'].get_moisture()
+
+        payload = '{"mac_address": "00:00:00:00:00:00", ' \
+                  '"temperature": "22", ' \
+                  f'"humidity": "{humidity}", ' \
+                  '"pressure": "888", ' \
+                  '"adc": "4.1"}'
+
+        iot_mod.send_POST_request(url=f'{config.API_URL}', payload=payload, content='JSON')
+        time.sleep(60 * 5)
+
+    iot_mod.deinitialize_serial()
